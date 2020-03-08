adaff583b609dfebe4882c1dbb7d5c2c3aed4bf3
geetha <geethainfy@gmail.com>
2020-03-08 12:34:07 +0530

    Add project files.

---
+++ src/TennisBookings.Web/Startup.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Web.Core.DependencyInjection;
+using TennisBookings.Web.Data;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using TennisBookings.Web.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace TennisBookings.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TennisBookingDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            //binds matching property from featurs:homepage in appsetting to homepageconfiguration properties
+            services.Configure<HomePageConfiguration>(Configuration.GetSection("Features:HomePage"));
+
+            //to validate the config binded value
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions<HomePageConfiguration>,
+                HomePageConfigurationValidation>());
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions<ExternalServicesConfig>, ExternalServicesConfigurationValidation>());
+            //to throw error irrespective of the page where the config is required eg contact page if accessed
+            // and if homepage config is invalid then error won't be thrown but with below method
+            //app woudl be stopped if we access even contact page where the home page config is not required
+            services.AddHostedService<ValidateOptionsService>();
+
+            services.Configure<GreetingConfiguration>(Configuration.GetSection("Features:Greeting"));
+
+            services.Configure<WeatherForecastingConfiguration>(Configuration.GetSection("Features:WeatherForecasting"));
+            //named config section binding
+            services.Configure<ExternalServicesConfig>(ExternalServicesConfig.WeatherApi, Configuration.GetSection("ExternalServices:WeatherApi"));
+            services.Configure<ExternalServicesConfig>(ExternalServicesConfig.ProductsApi, Configuration.GetSection("ExternalServices:ProductsApi"));
+
+            services.Configure<ContentConfiguration>(Configuration.GetSection("Content"));
+            services.AddSingleton<IContentConfiguration>(sp =>
+                sp.GetRequiredService<IOptions<ContentConfiguration>>().Value);
+
+            services
+                .AddAppConfiguration(Configuration)
+                .AddBookingServices()
+                .AddBookingRules()
+                .AddCourtUnavailability()
+                .AddMembershipServices()
+                .AddStaffServices()
+                .AddCourtServices()
+                .AddWeatherForecasting(Configuration)
+                .AddExternalProducts()
+                .AddNotifications()
+                .AddGreetings()
+                .AddCaching()
+                .AddTimeServices()
+                .AddAuditing()
+                .AddContentServices();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AuthorizePage("/FindAvailableCourts");
+                options.Conventions.AuthorizePage("/BookCourt");
+                options.Conventions.AuthorizePage("/Bookings");
+            });
+
+            services.AddIdentity<TennisBookingsUser, TennisBookingsRole>()
+                .AddEntityFrameworkStores<TennisBookingDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
